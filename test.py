1d7a0d65bcd66ee86712fcaa2867373030cd3497
Sumin Byeon <suminb@gmail.com>
2013-09-27 23:13:41 -0700

    Basic unit test.

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
+from testify import *
+from winnow import *
 
+class DefaultTestCase(TestCase):
+    def test_fingerprinting(self):
+        actual = winnow('A do run run run, a do run run')
+        expected = set([(5, 23942), (14, 2887), (2, 1966), (9, 23942), (20, 1966)])
+        assert_equal(actual, expected)
+
+if __name__ == "__main__":
+    run()
